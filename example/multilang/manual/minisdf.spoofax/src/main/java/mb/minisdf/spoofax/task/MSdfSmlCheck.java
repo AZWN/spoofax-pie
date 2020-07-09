d7a6ee1d62cd2c2153bcf915f952bd014734b822
Aron Zwaan <aronzwaan@gmail.com>
2020-07-09 17:26:00 +0200

    Update check tasks

--- example/multilang/manual/minisdf.spoofax/src/main/java/mb/minisdf/spoofax/task/MSdfSmlCheck.java
+++ example/multilang/manual/minisdf.spoofax/src/main/java/mb/minisdf/spoofax/task/MSdfSmlCheck.java
@@ -5,11 +5,13 @@ import mb.common.message.KeyedMessagesBuilder;
 import mb.common.message.Message;
 import mb.common.message.Messages;
 import mb.common.message.Severity;
+import mb.common.result.Result;
 import mb.pie.api.ExecContext;
 import mb.pie.api.MixedSession;
 import mb.pie.api.Pie;
 import mb.pie.api.ResourceStringSupplier;
 import mb.pie.api.Supplier;
+import mb.pie.api.Task;
 import mb.pie.api.TaskDef;
 import mb.resource.hierarchical.ResourcePath;
 import mb.spoofax.core.language.LanguageScope;
@@ -18,6 +20,7 @@ import mb.statix.multilang.LanguageId;
 import mb.statix.multilang.MultiLangAnalysisException;
 import mb.statix.multilang.pie.SmlBuildContextConfiguration;
 import mb.statix.multilang.pie.SmlBuildMessages;
+import mb.statix.multilang.pie.TaskUtils;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -44,7 +47,7 @@ public class MSdfSmlCheck implements TaskDef<ResourcePath, KeyedMessages> {
     }
 
     @Override
-    public KeyedMessages exec(ExecContext context, ResourcePath projectPath) throws Exception {
+    public KeyedMessages exec(ExecContext context, ResourcePath projectPath) {
         // Aggregate all parse messages
         final KeyedMessagesBuilder builder = new KeyedMessagesBuilder();
         analysisContextService.getLanguageMetadata(new LanguageId("mb.minisdf"))
@@ -60,32 +63,28 @@ public class MSdfSmlCheck implements TaskDef<ResourcePath, KeyedMessages> {
             });
 
         // Aggregate all Analysis Messages
-        final SmlBuildContextConfiguration.Output contextInfo = context.require(buildContextConfiguration.createTask(
-            new SmlBuildContextConfiguration.Input(projectPath, new LanguageId("mb.minisdf"))
-        ));
-
-        final List<LanguageId> languageIds = contextInfo.getContextConfig().getLanguages();
-        // We execute the actual analysis in the context of a shared Pie, to make sure all information is present.
-        // This will not break incrementality, because this task (or its equivalents for other languages)
-        // depends directly on all the files SmlBuildMessages depends on:
-        // - language source files:     via parse tasks
-        // - multilang.yaml:            via buildContextConfiguration tasks
-        final Pie sharedPie = analysisContextService.buildPieForLanguages(languageIds);
-        try(MixedSession session = sharedPie.newSession()) {
-            builder.addMessages(session.require(buildMessages.createTask(new SmlBuildMessages.Input(
-                projectPath,
-                languageIds,
-                contextInfo.getContextConfig().parseLevel()
-            ))));
-        }
-        return builder.build();
-    }
-
-    private Messages getMessages(ExecContext context, Supplier<Messages> msgSupplier) {
-        try {
-            return context.require(msgSupplier);
-        } catch(IOException e) {
-            throw new MultiLangAnalysisException(e);
-        }
+        return context.require(buildContextConfiguration.createTask(new SmlBuildContextConfiguration.Input(projectPath, new LanguageId("mb.minisdf"))))
+            .mapErr(MultiLangAnalysisException::new)
+            .flatMap(contextInfo -> {
+                final List<LanguageId> languageIds = contextInfo.getContextConfig().getLanguages();
+                // We execute the actual analysis in the context of a shared Pie, to make sure all information is present.
+                // This will not break incrementality, because this task (or its equivalents for other languages)
+                // depends directly on all the files SmlBuildMessages depends on:
+                // - language source files:     via parse tasks
+                // - multilang.yaml:            via buildContextConfiguration tasks
+                final Pie sharedPie = analysisContextService.buildPieForLanguages(languageIds);
+                try(MixedSession session = sharedPie.newSession()) {
+                    final Task<KeyedMessages> messagesTask = buildMessages.createTask(new SmlBuildMessages.Input(
+                        projectPath,
+                        languageIds,
+                        contextInfo.getContextConfig().parseLevel()
+                    ));
+                    return TaskUtils.executeWrapped(() -> Result.ofOk(session.require(messagesTask)), "Exception executing analysis");
+                }
+            })
+            .mapOrElse((KeyedMessages messages) -> {
+                builder.addMessages(messages);
+                return builder.build();
+            }, MultiLangAnalysisException::toKeyedMessages);
     }
 }
--- example/multilang/manual/ministr.spoofax/src/main/java/mb/ministr/spoofax/task/MStrSmlCheck.java
+++ example/multilang/manual/ministr.spoofax/src/main/java/mb/ministr/spoofax/task/MStrSmlCheck.java
@@ -4,17 +4,21 @@ import mb.common.message.KeyedMessages;
 import mb.common.message.KeyedMessagesBuilder;
 import mb.common.message.Messages;
 import mb.common.message.Severity;
+import mb.common.result.Result;
 import mb.pie.api.ExecContext;
 import mb.pie.api.MixedSession;
 import mb.pie.api.Pie;
 import mb.pie.api.ResourceStringSupplier;
+import mb.pie.api.Task;
 import mb.pie.api.TaskDef;
 import mb.resource.hierarchical.ResourcePath;
 import mb.spoofax.core.language.LanguageScope;
 import mb.statix.multilang.AnalysisContextService;
 import mb.statix.multilang.LanguageId;
+import mb.statix.multilang.MultiLangAnalysisException;
 import mb.statix.multilang.pie.SmlBuildContextConfiguration;
 import mb.statix.multilang.pie.SmlBuildMessages;
+import mb.statix.multilang.pie.TaskUtils;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -40,7 +44,7 @@ public class MStrSmlCheck implements TaskDef<ResourcePath, KeyedMessages> {
     }
 
     @Override
-    public KeyedMessages exec(ExecContext context, ResourcePath projectPath) throws Exception {
+    public KeyedMessages exec(ExecContext context, ResourcePath projectPath) {
         // Aggregate all parse messages
         final KeyedMessagesBuilder builder = new KeyedMessagesBuilder();
         analysisContextService.getLanguageMetadata(new LanguageId("mb.ministr"))
@@ -56,24 +60,28 @@ public class MStrSmlCheck implements TaskDef<ResourcePath, KeyedMessages> {
             });
 
         // Aggregate all Analysis Messages
-        final SmlBuildContextConfiguration.Output contextInfo = context.require(buildContextConfiguration.createTask(
-            new SmlBuildContextConfiguration.Input(projectPath, new LanguageId("mb.ministr"))
-        ));
-
-        final List<LanguageId> languageIds = contextInfo.getContextConfig().getLanguages();
-        // We execute the actual analysis in the context of a shared Pie, to make sure all information is present.
-        // This will not break incrementality, because this task (or its equivalents for other languages)
-        // depends directly on all the files SmlBuildMessages depends on:
-        // - language source files:     via parse tasks
-        // - multilang.yaml:            via buildContextConfiguration tasks
-        final Pie sharedPie = analysisContextService.buildPieForLanguages(languageIds);
-        try(MixedSession session = sharedPie.newSession()) {
-            builder.addMessages(session.require(buildMessages.createTask(new SmlBuildMessages.Input(
-                projectPath,
-                languageIds,
-                contextInfo.getContextConfig().parseLevel()
-            ))));
-        }
-        return builder.build();
+        return context.require(buildContextConfiguration.createTask(new SmlBuildContextConfiguration.Input(projectPath, new LanguageId("mb.ministr"))))
+            .mapErr(MultiLangAnalysisException::new)
+            .flatMap(contextInfo -> {
+                final List<LanguageId> languageIds = contextInfo.getContextConfig().getLanguages();
+                // We execute the actual analysis in the context of a shared Pie, to make sure all information is present.
+                // This will not break incrementality, because this task (or its equivalents for other languages)
+                // depends directly on all the files SmlBuildMessages depends on:
+                // - language source files:     via parse tasks
+                // - multilang.yaml:            via buildContextConfiguration tasks
+                final Pie sharedPie = analysisContextService.buildPieForLanguages(languageIds);
+                try(MixedSession session = sharedPie.newSession()) {
+                    final Task<KeyedMessages> messagesTask = buildMessages.createTask(new SmlBuildMessages.Input(
+                        projectPath,
+                        languageIds,
+                        contextInfo.getContextConfig().parseLevel()
+                    ));
+                    return TaskUtils.executeWrapped(() -> Result.ofOk(session.require(messagesTask)), "Exception executing analysis");
+                }
+            })
+            .mapOrElse((KeyedMessages messages) -> {
+                builder.addMessages(messages);
+                return builder.build();
+            }, MultiLangAnalysisException::toKeyedMessages);
     }
 }
