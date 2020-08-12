c49558740e76805366c6c01d6042ebb6a5f134de
Aron Zwaan <aronzwaan@gmail.com>
2020-08-12 13:22:58 +0200

    Use new metadata packages

--- example/multilang/manual/minisdf.spoofax/src/main/java/mb/minisdf/spoofax/MiniSdfComponent.java
+++ example/multilang/manual/minisdf.spoofax/src/main/java/mb/minisdf/spoofax/MiniSdfComponent.java
@@ -4,7 +4,7 @@ import dagger.Component;
 import mb.spoofax.core.language.LanguageComponent;
 import mb.spoofax.core.language.LanguageScope;
 import mb.spoofax.core.platform.PlatformComponent;
-import mb.statix.multilang.LanguageMetadataProvider;
+import mb.statix.multilang.metadata.LanguageMetadataProvider;
 import mb.statix.multilang.MultiLangComponent;
 
 @LanguageScope
--- example/multilang/manual/ministr.eclipse/src/main/java/mb/ministr/eclipse/MiniStrMetadataProvider.java
+++ example/multilang/manual/ministr.eclipse/src/main/java/mb/ministr/eclipse/MiniStrMetadataProvider.java
@@ -1,8 +1,8 @@
 package mb.ministr.eclipse;
 
-import mb.statix.multilang.ContextId;
-import mb.statix.multilang.LanguageId;
-import mb.statix.multilang.LanguageMetadata;
+import mb.statix.multilang.metadata.ContextId;
+import mb.statix.multilang.metadata.LanguageId;
+import mb.statix.multilang.metadata.LanguageMetadata;
 import mb.statix.multilang.eclipse.LanguageMetadataProvider;
 
 import java.util.HashMap;
